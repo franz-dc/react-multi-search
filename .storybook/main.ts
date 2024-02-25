fe0735d1c00ccae93f9040980a70ef41c417832d
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-02-26 01:13:01 +0800

    chore: remove unused addon-onboarding from Storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
+    // '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
   framework: {
